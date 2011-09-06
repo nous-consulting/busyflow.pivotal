e308fa26e76ad7729d0cb5aef072629fe1962501
Ignas Mikalajūnas <ignas.mikalajunas@gmail.com>
2011-09-06 17:38:16 +0200

    Bumping version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='busyflow.pivotal',
-    version='0.1.2',
+    version='0.1.3',
     description='Pivotal API client library.',
     author='Ignas Mikalajunas',
     author_email='ignas@nous.lt',
